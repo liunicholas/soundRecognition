7504249da1fd74a3ccdfd3425365bd45107dfc6f
Nicholas Liu <51180693+liunicholas@users.noreply.github.com>
2021-09-02 14:03:14 -0700

    Update tfSoundTutorial.py

--- tfSoundTutorial.py
+++ tfSoundTutorial.py
@@ -84,7 +84,7 @@ def get_spectrogram(waveform):
 
  #building and training model
 
- def preprocess_dataset(files):
+def preprocess_dataset(files):
   files_ds = tf.data.Dataset.from_tensor_slices(files)
   output_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
   output_ds = output_ds.map(
