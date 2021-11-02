01ac85770a83fa69a821f46e116ebb4138d62881
Claire <clairezhaolj@gmail.com>
2021-11-02 14:10:15 -0700

    Update 3dcnn.py

--- 3dcnn.py
+++ 3dcnn.py
@@ -1,41 +1,96 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import tensorflow
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv3D, MaxPooling3D
+from tensorflow.keras.utils import to_categorical
+import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import random
-import h5py
 
-#source test data: https://www.kaggle.com/daavoo/3d-mnist
 
- with h5py.File("../input/train_point_clouds.h5", "r") as hf:
-     X_train = hf["X_train"][:]
-     y_train = hf["y_train"][:]
-     X_test = hf["X_test"][:]
-     y_test = hf["y_test"][:]
+# -- Preparatory code --
+# Model configuration
+batch_size = 100
+no_epochs = 30
+learning_rate = 0.001
+no_classes = 10
+validation_split = 0.2
+verbosity = 1
 
-"""
-sample code for 3d?
-model = Sequential([
-    layers.Conv3D(8, (3,3,3), activation='relu', padding='same', input_shape=(1, 16,16,16)),
-    layers.Conv3D(16, (3,3,3), activation='relu', padding='same'),
-    layers.MaxPooling3D((2,2,2), padding='same'),
 
-    layers.Conv3D(32, (3,3,3), activation='relu', padding='same'),
-    layers.Conv3D(64, (3,3,3), activation='relu', padding='same'),
-    layers.MaxPooling3D((2,2,2), padding='same'),
+# Convert 1D vector into 3D values, provided by the 3D MNIST authors at
+# https://www.kaggle.com/daavoo/3d-mnist
+def array_to_color(array, cmap="Oranges"):
+  s_m = plt.cm.ScalarMappable(cmap=cmap)
+  return s_m.to_rgba(array)[:,:-1]
 
-    layers.Conv3D(16, (3,3,3), activation='relu', padding='same'),
-    layers.BatchNormalization(),
-    layers.GlobalAveragePooling3D(),
-    layers.Flatten(),
+# Reshape data into format that can be handled by Conv3D layers.
+# Courtesy of Sam Berglin; Zheming Lian; Jiahui Jang - University of Wisconsin-Madison
+# Report - https://github.com/sberglin/Projects-and-Papers/blob/master/3D%20CNN/Report.pdf
+# Code - https://github.com/sberglin/Projects-and-Papers/blob/master/3D%20CNN/network_final_version.ipynb
+def rgb_data_transform(data):
+  data_t = []
+  for i in range(data.shape[0]):
+    data_t.append(array_to_color(data[i]).reshape(16, 16, 16, 3))
+  return np.asarray(data_t, dtype=np.float32)
 
-    layers.Dense(units=1024, activation='relu'),
-    layers.Dropout(0.4),
-    layers.Dense(units=256, activation='relu'),
-    layers.Dropout(0.4),
-    layers.Dense(units=10, activation='softmax'),
-])
-"""
+
+
+# -- Process code --
+# Load the HDF5 data file
+with h5py.File("./full_dataset_vectors.h5", "r") as hf:
+
+    # Split the data into training/test features/targets
+    X_train = hf["X_train"][:]
+    targets_train = hf["y_train"][:]
+    X_test = hf["X_test"][:]
+    targets_test = hf["y_test"][:]
+
+    # Determine sample shape
+    sample_shape = (16, 16, 16, 3)
+
+    # Reshape data into 3D format
+    X_train = rgb_data_transform(X_train)
+    X_test = rgb_data_transform(X_test)
+
+    # Convert target vectors to categorical targets
+    targets_train = to_categorical(targets_train).astype(np.integer)
+    targets_test = to_categorical(targets_test).astype(np.integer)
+
+
+# Create the model
+model = Sequential()
+model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=sample_shape))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(Flatten())
+model.add(Dense(256, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(no_classes, activation='softmax'))
+
+
+# Compile the model
+model.compile(loss=tensorflow.keras.losses.categorical_crossentropy,
+              optimizer=tensorflow.keras.optimizers.Adam(lr=learning_rate),
+              metrics=['accuracy'])
+
+# Fit data to model
+history = model.fit(X_train, targets_train,
+            batch_size=batch_size,
+            epochs=no_epochs,
+            verbose=verbosity,
+            validation_split=validation_split)
+
+# Generate generalization metrics
+score = model.evaluate(X_test, targets_test, verbose=0)
+print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+
+# Plot history: Categorical crossentropy & Accuracy
+plt.plot(history.history['loss'], label='Categorical crossentropy (training data)')
+plt.plot(history.history['val_loss'], label='Categorical crossentropy (validation data)')
+plt.plot(history.history['accuracy'], label='Accuracy (training data)')
+plt.plot(history.history['val_accuracy'], label='Accuracy (validation data)')
+plt.title('Model performance for 3D MNIST Keras Conv3D example')
+plt.ylabel('Loss value')
+plt.xlabel('No. epoch')
+plt.legend(loc="upper left")
+plt.show()
