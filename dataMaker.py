9aec2d55aab81fb62c8348a0c8ea8c6a1a9beaec
Nicholas Liu <51180693+liunicholas@users.noreply.github.com>
2021-11-30 14:43:32 -0800

    fix dataMaker.py

--- dataMaker.py
+++ dataMaker.py
@@ -12,17 +12,23 @@ def main():
     global counter
     folders = os.listdir(directory)
     for folder in folders:
-        for wavFile in folder:
-            print(f"\ngathering data for wav file {counter}")
-            filePath = f"{directory}/{folder}/{wavFile}"
-            fourDspec = spectrogramConverter.convertSpectrogram(filePath)
-            fourDspec = np.array(fourDspec)
+        for filename in os.listdir(f"{directory}/{folder}"):
+            if filename.endswith(".wav"):
+                print(f"\ngathering data for wav file {counter}")
+                filePath = f"{directory}/{folder}/{filename}"
+                print(f"{filePath}")
+                fourDspec = spectrogramConverter.convertSpectrogram(filePath)
+                fourDspec = np.array(fourDspec)
 
-            newFileName = Path(wavFile).stem
-            with open(f"{dataPath}/{folder}/{newFileName}", 'wb') as f:
-                np.save(f, fourDspec)
+                newFileName = Path(filename).stem
+                
+                if not os.path.exists(f"{dataPath}/{folder}"):
+                    os.makedirs(f"{dataPath}/{folder}")
 
-            counter+=1
+                with open(f"{dataPath}/{folder}/{newFileName}", 'wb') as f:
+                    np.save(f, fourDspec)
+
+                counter+=1
 
 if __name__ == '__main__':
 	main()
