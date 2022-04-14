f9b703578f30b2425b3fdfe087449ad5881e5ef3
Nicholas Liu <51180693+liunicholas@users.noreply.github.com>
2022-04-14 12:40:51 -0700

    tested transcription package

--- parameters.py
+++
@@ -1,5 +0,0 @@
-dataPath = "./spectrogramData"
-
-savedPlotsPath = "./plots"                   #saves plot
-savedModelsPath = "./savedModels"            #save best model
-checkpointPath = "./checkpointModels"
